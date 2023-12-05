817430f0ecb8e87b6dbdb13ff174afd2a1a46055
Muhammad Syamil Hamami <msyamil.hamami@gmail.com>
2023-12-05 16:16:55 +0700

    Fix path

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import path from "path"
 import express from "express"
 import expressLayouts from "express-ejs-layouts"
 import session from "express-session"
@@ -17,7 +18,7 @@ import {
 const app = express()
 
 app.set("view engine", "ejs")
-app.set("views", "./src/views")
+app.set("views", path.join(path.resolve(), "src/views"))
 
 app.use(expressLayouts)
 app.use(express.json())
